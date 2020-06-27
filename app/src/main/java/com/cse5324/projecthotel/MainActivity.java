02c5c2f2e3f6233741c49f165679cc73b32ad8ce
RoyRod93 <rroysten@gmail.com>
2020-06-27 15:43:27 -0500

    Updated Login Screen Created Register Screen

---
+++ app/src/main/java/com/cse5324/projecthotel/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cse5324.projecthotel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
